aae0ba05b978734b578c641a6bae0c4bed233d1f
CMAREQ <temomareq@gmail.com>
2017-10-25 14:39:39 -0500

    el travis y el ivan

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("HOLAHOLA")
+import unittest
+from cosa import birthdayCakeCandles
+
+class TestCake(unittest.TestCase):
+
+    def testCandles(self):
+        self.assertEqual(birthdayCakeCandles(5, [2, 2, 2, 2, 2]), 5)
+
+if __name__ == '__main__':
+    unittest.main()
