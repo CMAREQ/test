e251a68eef147be14bda9a9abbbb94c13e6768cf
CMAREQ <temomareq@gmail.com>
2017-10-25 14:48:22 -0500

    el travis y el ivan

--- cosa.py
+++ cosa.py
@@ -2,21 +2,19 @@
 import sys
 
 def birthdayCakeCandles(n, ar):
-    #la lista es acomodada de forma decendente
+
     ar.sort(reverse=True)
 
     max = ar[0]
-    #se inicializa un contador de las velas que tengan el mismo tamaño
     c = 0
     for objeto in ar:
-        #por cada núm de la lista:
-        #si el tamaño maximo es igual con el actual, lo cuenta
+
         if max == objeto:
             c = c + 1
-        #si ya no coinciden, se termina el ciclo
+
         else:
             break
-    #regresa la cuenta
+
     return c
 
 if __name__ == '__main__':
